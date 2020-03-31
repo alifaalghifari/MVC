930bf794f033bca34971e4c59441aec2b9ae9252
alif_alghifari <alifalghifari798@gmail.com>
2020-03-31 10:22:52 +0800

    membuat file .htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -5,6 +5,15 @@ class App
 
     public function __construct()
     {
-        echo __DIR__ . __CLASS__;
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if (isset($_GET['url'])) {
+            $url = $_GET['url'];
+            return $url;
+        }
     }
 }
